bab9fffdc478fae7d1b64e4ae06b408baceedcd2
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2022-05-27 15:44:58 +0530

    1st file added

---
+++ distance.py
@@ -0,0 +1,9 @@
+def Dist(x1,x2,y1,y2):
+    return (((x2-x1)**2 + (y2-y1)**2)**0.5)
+
+x1=int(input("x1="))
+x2=int(input("x2="))
+y1=int(input("y1="))
+y2=int(input("y2="))
+print(Dist(x1,x2,y1,y2))
+
